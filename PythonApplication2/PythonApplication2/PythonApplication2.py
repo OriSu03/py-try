632c788b70126d3b895d0b11ef523cf8a2cdc39c
616085839 <616085839@qq.com>
2018-10-16 15:37:09 +0800

    make_album

--- PythonApplication2/PythonApplication2/PythonApplication2.py
+++ PythonApplication2/PythonApplication2/PythonApplication2.py
@@ -1,3 +1,8 @@
 def make_album(singer_name,album_name,number=''):
+    pt={'singer':singer_name,'album':album_name}
+    if number:
+        pt['number']=number
+    return pt
 
-
+ptt=make_album('lee','ailee')
+print(ptt)
